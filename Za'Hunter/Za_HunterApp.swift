8dd1fea8a810e9608c936a839da0a057baf1efb5
Srishti Kamra <srishtikamra07@gmail.com>
2023-02-13 09:46:06 -0600

    Initial Commit

---
+++ Za'Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za'Hunter
+//
+//  Created by Srishti Kamra  on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
