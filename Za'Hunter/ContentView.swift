524bd1545bb472ab3c57df3c3919186acc9bef92
Srishti Kamra <srishtikamra07@gmail.com>
2023-02-22 09:54:44 -0600

    Added map search, pizza icons and link to store website

--- Za'Hunter/ContentView.swift
+++ Za'Hunter/ContentView.swift
@@ -1,42 +1,73 @@
-//
-//  ContentView.swift
-//  Za'Hunter
-//
-//  Created by Srishti Kamra  on 2/13/23.
-//
-
 import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @State private var places = [Place]()
     @StateObject var locationManager = LocationManager()
-       @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var region = MKCoordinateRegion(
-            center: CLLocationCoordinate2D(
-                latitude: 42.15704,
-                longitude: -88.14812),
-            span: MKCoordinateSpan(
-                latitudeDelta: 0.05,
-                longitudeDelta: 0.05)
-        )
+        center: CLLocationCoordinate2D(
+            latitude: 42.15704,
+            longitude: -88.14812),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05)
+    )
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Map(
-                       coordinateRegion: $region,
-                       interactionModes: .all,
-                       showsUserLocation: true,
-                       userTrackingMode: $userTrackingMode
-                   )
+                coordinateRegion: $region,
+                interactionModes: .all,
+                showsUserLocation: true,
+                userTrackingMode: $userTrackingMode,
+                annotationItems: places) { place in
+                    MapAnnotation(coordinate: place.annotation.coordinate) {
+                        Marker(mapItem: place.mapItem)
+                    }              }
+                .onAppear(perform: {
+                    performSearch(item: "Pizza")
+                } )
         }
-      
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { response, error in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                }
+            }
+        }
+    }
+    
+    struct Marker: View {
+        var mapItem: MKMapItem
+        var body: some View {
+            if let url = mapItem.url {
+                Link(destination: url, label: {
+                    Image("pizza")
+                })
+            }
+        }
+    }
+    
+    struct Place: Identifiable {
+        let id = UUID()
+        let annotation: MKPointAnnotation
+        let mapItem: MKMapItem
     }
 }
